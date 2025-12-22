5e08132fdcb434000cd0b416f81d8fe8b2d440a9
68219090014-byte <68219090014@technictrang.ac.th>
2025-12-22 11:58:02 +0700

    Update LCD_Hello_world.ino

--- offline/LCD/LCD_Hello_world.ino
+++ offline/LCD/LCD_Hello_world.ino
@@ -1,14 +1,25 @@
+#8.1.2.1
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-   lcd.init();
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
+  lcd.print("LAB 8.1.2 LCD ");
+  lcd.setCursor(0, 1);
+  lcd.print("Saharat/chanpakorn");
+
+  delay(4000);
+  lcd.clear();
 }
 
 void loop() {
-  lcd.setCursor(0, 0);
-  lcd.print("8.1.2 ");
-  lcd.setCursor(0, 1);
-  lcd.print("Hello world");
+  lcd.setCursor(4, 0);
+  lcd.print("Hello");
+  lcd.setCursor(4, 1);
+  lcd.print("world");
 }
